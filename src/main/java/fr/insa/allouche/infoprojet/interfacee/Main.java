a2f9ad30774e14032969d800fe5b5c5835db804c
Portable <Portable@192.168.0.12>
2021-04-16 18:39:33 +0200

    rajout pakage interface + classe main pour test puch et point départ Ali

--- src/main/java/fr/insa/allouche/infoprojet/interfacee/Main.java
+++ src/main/java/fr/insa/allouche/infoprojet/interfacee/Main.java
@@ -3,12 +3,34 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package fr.insa.allouche.infoprojet.interfacee;
 
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 /**
  *
  * @author Portable
  */
-public class Main {
-    
-}
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("Dessin 2D");
+//      Scene scene = new Scene(new MainDessinPane());
+        //Groupe model = Groupe.groupeTest();
+        //Scene scene = new Scene(new MainDessinPaneResizable(model));
+       // primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}    
+
