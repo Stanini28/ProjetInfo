f251dd5c7448c727440e9b8274b8817b6b974a0d
Portable <Portable@192.168.0.12>
2021-04-18 19:29:09 +0200

    Creation classe demandeNoeud et lien UML

--- src/main/java/fr/insa/allouche/infoprojet/Noeud.java
+++ src/main/java/fr/insa/allouche/infoprojet/Noeud.java
@@ -5,17 +5,41 @@
  */
 package fr.insa.allouche.infoprojet;
 
-import java.util.Set;
+import fr.insa.allouche.infoprojet.outils.Lire;
+//import java.util.Set;
 
 
 public class Noeud {
 
     private int id;
-    private double px;
-    private double py;
+    private Point position;
     private Poids P;
+    private Treillis contient;
 
-    public Set<Barre> extremites;
+    //public Set<Barre> extremites;
+    
+    public Noeud (Point position){
+        this.position = new Point(position.getPX(),position.getPY());
+    }
+    
+    public Noeud() {
+        this.position= new Point();
+    }
+
+    
+    public Point getPosition() {
+        return position;
+    }
+
+    public Treillis getContient() {
+        return contient;
+    }
+    
+    void setPosition(Point position) {
+        this.position = position;
+    }
+    
+    
 
     public void sommeX() {
 
@@ -28,5 +52,21 @@ public class Noeud {
     public void solUnique() {
 
     }
+
+    
+
+    void setContient(Treillis contient) {
+        this.contient = contient;
+    }
+    
+    public Noeud demandeNoeud(){
+        Point noeud = new Point();
+        System.out.println("abscisse : ");
+        noeud.setPX(Lire.d());
+        System.out.println("ordonnée : ");
+        noeud.setPY(Lire.d());
+        return new Noeud(noeud);
+    }
+    
 }
 
