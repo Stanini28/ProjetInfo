63bd48134df5138324d50d63ec420a7c6256b0bd
Portable <Portable@192.168.0.12>
2021-04-18 19:35:48 +0200

    Constructeur et lien UML

--- src/main/java/fr/insa/allouche/infoprojet/TriangleTerrain.java
+++ src/main/java/fr/insa/allouche/infoprojet/TriangleTerrain.java
@@ -8,12 +8,45 @@ package fr.insa.allouche.infoprojet;
 
 
 public class TriangleTerrain {
-
-    public TriangleTerrain() {
+    
+    private int id;
+    private SegmentTerrain SegTerrain1;
+    private SegmentTerrain SegTerrain2;
+    private SegmentTerrain SegTerrain3;
+    
+    //reprendre
+    public TriangleTerrain(SegmentTerrain SegTerrain1, SegmentTerrain SegTerrain2,
+            SegmentTerrain SegTerrain3 ) {
+        
+        //pas sur que la classe identificateur attribut le bon identifiant
+        this.id= getId();
+        this.SegTerrain1 = SegTerrain1;
+        this.SegTerrain2 = SegTerrain2;
+        this.SegTerrain3 = SegTerrain3;
+        
+        //defini à quel Triangle terrain appartient les segment terrain
+        SegTerrain1.setFaitPartieDe(this);
+        SegTerrain2.setFaitPartieDe(this);
+        SegTerrain2.setFaitPartieDe(this);
     }
 
-    private int id;
+    public int getId() {
+        return id;
+    }
 
+    public SegmentTerrain getSegTerrain1() {
+        return SegTerrain1;
+    }
+
+    public SegmentTerrain getSegTerrain2() {
+        return SegTerrain2;
+    }
+
+    public SegmentTerrain getSegTerrain3() {
+        return SegTerrain3;
+    }
+    
+    
     public void angle() {
 
     }
