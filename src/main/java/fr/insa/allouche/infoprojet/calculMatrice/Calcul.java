aa0c555c5a128cea5e188d778ea33a8b595873e0
Portable <Portable@192.168.0.12>
2021-04-28 17:28:27 +0200

    Merge origin/master
    Conflicts:
    	src/main/java/fr/insa/allouche/infoprojet/Barre.java

--- src/main/java/fr/insa/allouche/infoprojet/calculMatrice/Calcul.java
+++ src/main/java/fr/insa/allouche/infoprojet/calculMatrice/Calcul.java
@@ -5,6 +5,7 @@
  */
 package fr.insa.allouche.infoprojet.calculMatrice;
 
+import fr.insa.allouche.infoprojet.Barre;
 import fr.insa.allouche.infoprojet.Noeud;
 import fr.insa.allouche.infoprojet.NoeudSimple;
 import fr.insa.allouche.infoprojet.Point;
@@ -16,29 +17,50 @@ import fr.insa.allouche.infoprojet.Point;
 public class Calcul {
 
     public static double pAngle(Point O, Point X, Noeud P, Noeud Q) {
-        double angle = Math.atan2(O.getPY() - X.getPY(), O.getPX() - X.getPX()) - Math.atan2(P.position.getPY() - Q.position.getPY(), P.position.getPX() - Q.position.getPX());
+        double angle = (Math.atan2(Q.position.getPY() - P.position.getPY(), Q.position.getPX() - P.position.getPX()) - Math.atan2(X.getPY() - O.getPY(), X.getPX() - O.getPX()));
 
-        angle = angle * Math.PI / 180;
         return angle;
 
-       
-    }
+    } // Fonctionne sûr sûr 
 
-    public void SommeNoeudS(NoeudSimple P) {
-        Point O= new Point(0,0);
-        Point X= new Point(0,1);
-        Point Y=new Point(1,0);
-        
-        
-
-        Matrice Mns = new Matrice(2, P.getLiee().size()+1);
+    public static Matrice SommeNoeudS(NoeudSimple P) {
+        Point O = new Point(0, 0);
+        Point X = new Point(1, 0);
+        Point Y = new Point(0, 1);
+        Matrice Mns = new Matrice(2, P.getLiee().size() + 1);
 
         for (int j = 0; j < P.getLiee().size(); j++) {
-            Mns.coeffs[0][j] = P.liee.get(j).Fx * Math.cos(pAngle(O, X, P, P.liee.get(j).getDebut())); //force de traction en X
-            Mns.coeffs[1][j] = P.liee.get(j).Fy * Math.sin(pAngle(O, X, P, P.liee.get(j).getDebut()));// Force de de traction en Y
 
-            Mns.coeffs[1][P.getLiee().size()+1] = P.forceY;
+            Mns.coeffs[0][j] = Math.cos(pAngle(O, X, P, P.getLiee().get(j).getFin())); //force de traction en X
+            Mns.coeffs[1][j] = Math.sin(pAngle(O, X, P, P.getLiee().get(j).getFin()));// Force de de traction en Y
+
+            Mns.coeffs[1][P.getLiee().size()] = P.forceY;
+
         }
+
+        return Mns;
+    }
+
+    public static void main(String[] args) {
+        Point pos1 = new Point(3, 2);
+        Point pos2 = new Point(6, 3);
+        Point O = new Point(0, 0);
+        Point X = new Point(1, 0);
+        Point pos5 = new Point(-1, -1);
+        NoeudSimple nS1 = new NoeudSimple(pos1);
+        NoeudSimple nS2 = new NoeudSimple(pos2);
+        //NoeudSimple nS4 = new NoeudSimple(pos4);
+        NoeudSimple nS5 = new NoeudSimple(pos5);
+        Barre b1 = new Barre(nS1, nS2);
+        Barre b2 = new Barre (nS1, nS5);
+        nS1.forceY = 9;
+
+        Matrice M = SommeNoeudS(nS1);
+
+  
+                System.out.println(M.toString());
+            
+        
 
     }
 
