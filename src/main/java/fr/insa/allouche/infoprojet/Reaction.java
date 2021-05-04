5899aa621b68937c43f40453a66118673e7acdb8
stanislasallouche <stanislas.allouche@insa-strasbourg.fr>
2021-05-04 21:54:11 +0200

    Changement nom classe

---
+++ src/main/java/fr/insa/allouche/infoprojet/Reaction.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.insa.allouche.infoprojet;
+
+public class Reaction {
+
+    private int idRx;
+    private int idRy;
+    private double Rx;
+    private double Ry;
+
+    public int getIdRx() {
+        return idRx;
+    }
+
+    public int getIdRy() {
+        return idRy;
+    }
+
+    public double getRx() {
+        return Rx;
+    }
+
+    public double getRy() {
+        return Ry;
+    }
+
+    public void setIdRx(int idRx) {
+        this.idRx = idRx;
+    }
+
+    public void setIdRy(int idRy) {
+        this.idRy = idRy;
+    }
+
+    public void setRx(double Rx) {
+        this.Rx = Rx;
+    }
+
+    public void setRy(double Ry) {
+        this.Ry = Ry;
+    }
+
+    
+    
+
+   
+   
+
+    
+    
+}
